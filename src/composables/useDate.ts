ae6f902eda263b62953eee29e5e16f367c53127f
Gabriel Guerra <gfranklings@gmail.com>
2022-06-29 12:04:26 -0400

    date fixes

--- src/composables/useDate.ts
+++ src/composables/useDate.ts
@@ -1,15 +1,20 @@
 import { ref, onMounted } from 'vue'
 
 export const useDate = () => {
-  const hour = ref<number>(new Date().getHours())
-  const minutes = ref<number>(new Date().getMinutes())
+  const hour = ref<number>(0)
+  const minutes = ref<number>(0)
 
   const refreshData = () => {
     hour.value = new Date().getHours()
     minutes.value = new Date().getMinutes()
+
+    if (minutes.value < 10) {
+      minutes.value = `0${minutes.value}`
+    }
   }
 
   onMounted(() => {
+    refreshData()
     setInterval(() => {
       refreshData()
     }, 1000)
